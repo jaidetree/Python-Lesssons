cc700459c5c375a50487e40058f03344ec62f785
Jay Zawrotny <jayzawrotny@gmail.com>
2011-09-24 04:59:44 -0400

    Rewrite the degrees fahrenheit to degrees celsius using variables.

---
+++ f_to_c2.py
@@ -0,0 +1,11 @@
+# In lession 4 I learned what variables are so yeah, rewriting it.
+# Calculates degress in fahrenheit to celsius
+
+# This is what we're taking, our input as it were. Used to calculate our output.
+degrees_farenheit = 75
+
+# Calculate our output.
+degrees_celsius = ( degrees_farenheit - 32 ) * 5 / 9.0
+
+# Output it.
+print "Degrees", degrees_farenheit, "fahrenheit to:", degrees_celsius , "celsius."
