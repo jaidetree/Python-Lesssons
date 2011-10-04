02ee88651953ac8cf99d431ef4dc90ddf7a685c5
Jay Zawrotny <jayzawrotny@gmail.com>
2011-10-04 00:56:23 -0400

    Finished my choose your own adventure thing. Let me know if you find any bugs.

--- ex36.py
+++ ex36.py
@@ -1,23 +1,26 @@
 from sys import exit
 
-prompt = "> "
 response = ""
 
 def show_question(prompt, options):
     """A reusable function for showing a room and getting a response."""
 
-    if isinstance(options, list):
+    if not isinstance(options, list):
+        print "Options is a: " + str(type(options))
         kill("Specified options argument is not a list type.")
 
-    break_loop = False
+    while True:
+        print "\n" + prompt
 
-    while not break_loop:
-        print prompt
+        print "\nYour choices are:\n"
 
-        input = raw_input(prompt)
+        for i in range(0, len(options)):
+            print "{}.) \"{}\"".format(i + 1, options[i])
 
-        for i in Range(0, options.count)
-            if input == options[i]:
+        input = raw_input("> ")
+
+        for i in range(0, len(options)):
+            if input == options[i] or ( input.isdigit() and int(input) - 1 == i):
                 return i
 
         print "That was not a valid response. Try again."
@@ -61,7 +64,7 @@ if response == 0:
     print "The naked goblin does not like the moon, he now comences clubbing you to death...with his penis."
     kill("...and a few harsh blows later you're cold lifeless body hugs the castle floor for eternity. Also, that naked goblin is probably going to defile your corpse. Just so you know...") 
 elif response == 1:
-    print "You lunge your right, iron-clad foot right into the naked goblin's junk. I think you killed him.Nice..."
+    print "You lunge your right, iron-clad foot right into the naked goblin's junk. I think you killed him. Nice..."
     situation = 1
 elif response == 2:
     print "Jesus christ! You just ripped that things head off, the poor bastard screamed so loud you've awaken the castle's ghosts which snatch you from the ground and throw you in a dungeon."
@@ -76,18 +79,58 @@ if situation == 2:
             "I want to die!!!"
             ]
 
-    response == show_question(question, options)
+    response = show_question(question, options)
 
     if response == 0:
         print "You have escaped. You head to the castle's tower."
-    elif repsonse == 1:
+    elif response == 1:
         print "In an angry plight of shame & self loathing you kill yourself or whatever."
         kill("Yeah, you're dead.")
+
+    situation = 0
     
 # Scenario 4: Damsel Rescue
 if situation == 1:
-    question = """On your way to the tower you see a damsel, she's not distressed, she just seems really bored.
+    question = """On your way to the tower you see a damsel, she's not distressed, she just seems really bored and is staring out a window with her back turned to you."
     Would what would you like to do about her?"""
 
     options = [
-            "
+            "I don't care",
+            "Offer her a cigarette",
+            "Try to start a conversation"
+            ]
+
+    response = show_question( question, options )
+
+    if response == 0:
+        print "You decide to ignore her and move on towards the top of the tower. Probably a safe bet this being a castle full of ghosts and naked goblins and all."
+    elif response == 1:
+        print "Sounding immencely delighted by your offer, you approach her, right as you reach to hand her one of your smokes she shanks you and takes your money." 
+        kill("Now bleeding and poor, no one wants anything to do with you. You're just left there to die.")
+    elif response == 2:
+        print "You try to ask her wassup but she just sighs and continues staring out the window. Defeated, you tronce forward."
+
+    response = 0
+
+# Scenario 5: Gold
+
+question = """You made it to the top of the tower! Upon endering the room you find out there's an eleveator which will take you to the front gate, oh and piles of gold just laying about. Curse the lazy slobs that dwelled here beforeth!
+Make your move."""
+
+options = [
+        "Take the gold",
+        "Leave",
+        "Go to bed"
+        ]
+
+response = show_question(question, options)
+
+if response == 1:
+    print "You take the gold, you leave the castle. You're a better man for it. Well played."
+elif repsonse == 2:
+    print "You leave but you didn't take the gold, which of course a duke comes and arrests you. This castle was forbidden and you have no gold to bribe him. You're just gonna rot in a jail cell somewhere."
+elif response == 3:
+    print "Not knowing what to do, you decide to go to bed. You mysteriously die in your sleep."
+    kill("It wasn't that mysterious, you were drunk and choked on your own vomit. Way to go Hendrix.") 
+
+print "You won the game. Sweet dreams!"
